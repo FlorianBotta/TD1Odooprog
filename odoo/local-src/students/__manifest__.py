b6becf6b40a59aa025174bb5c4dd82434c79322e
matski <florian.botta@gmail.com>
2023-03-10 16:39:32 +0100

    Commit TD1

---
+++ odoo/local-src/students/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Gestion des étudiants",
+    "version": "1.0",
+    "category": "Generic Modules/Others",
+    "description": """Test création module gestion des étudiants Odoo v14""",
+    "author": "BOTTA",
+    "depends": ["base"],
+    "data": [
+        "data/students_training_data.xml",
+        "data/students_student_data.xml",
+        "data/students_mark_data.xml",
+        "views/students_views.xml",
+        "views/trainings_views.xml",
+        "views/notes_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
